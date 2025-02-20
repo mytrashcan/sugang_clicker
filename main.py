06e490e05af7fc8ed5ea58575f198f75cb8aa26e
kitae <wjdrlxo2013@gmail.com>
2025-02-20 22:31:53 +0900

    fixed some commnets

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     try:
         app = SugangApplication()
         target_time = "09:59:58"  # 1, 2초 정도 빠르게 설정할것을 추천(안정적: 1초, 빠르게: 2초)
-        app.run(target_time, interval=0.05)
+        app.run(target_time, interval=0.05) # 수강 신청 버튼 클릭 간 0.05초 간격으로 실행
     except Exception as e:
         print(f"프로그램 실행 중 오류 발생: {e}")
     finally:
